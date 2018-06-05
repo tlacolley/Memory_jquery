f345ea140a6ff4c2dcef9f6916ada706cfdd9fed
tlacolley <thomaslacolley@gmail.com>
2018-06-05 12:29:18 +0200

    recherche JS, recherche contrainte 2 events.

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ var back = document.getElementsByClassName("back");
 var hidden = document.getElementsByClassName("gne");
 
 for (var i = 0; i < back.length; i++) {
-    back[i].addEventListener("click", flip);
 
-    function flip(){
-    this.style.display = "none";
-    // this.style.toggleClass(.back.active);
+        function flip(){
+
+            var numFlip = 0;
+            if(numFlip > 3){
+                this.style.visibility = "hidden";
+                numFlip += 1;
+                // this.style.toggleClass(.back.active);
+
+                    }
+
+                }
+
+         back[i].addEventListener("click", flip);
+
+
+
+
     }
-
-}
