3f1fa3d27c090fe2e7804cb26c6cafc0c7692601
Caroline <c.bove16@gmail.com>
2018-06-21 16:29:26 +0200

    Shuffle, click pour retourner cartes

--- script.js
+++ script.js
@@ -1,109 +1,89 @@
-
-
-$(document).ready(function(){
-
-    var divBack = $("<div>")
-    var divFace = $("<div>")
-    divBack.addClass("divBack")
-    divFace.addClass("divFace")
+$(document).ready(function() {
+    var tableCards = [];
+    var divBack = $("<div>");
+    var divFace = $("<div>");
+    divBack.addClass("divBack");
+    divFace.addClass("divFace");
     $("main").append(divBack);
     $("main").append(divFace);
 
     divBack.css({
-        "display":"flex",
+        "display": "flex",
         "position": "absolute",
         "margin": "1%",
-        "flexWrap":"wrap",
+        "flexWrap": "wrap",
     })
 
     divFace.css({
-        "display":"flex",
+        "display": "flex",
         "position": "absolute",
         "margin": "1%",
-        "flexWrap":"wrap",
+        "flexWrap": "wrap",
     })
 
-    $("main").css("position","relative")
+    $("main").css("position", "relative")
 
 
 
     var nbCard = prompt("enter nombres cartes paire !");
 
-    while (nbCard %2){
+    while (nbCard % 2) {
         nbCard = prompt("PAIRE MOTHERFUCKER!!!!!");
     }
 
-    for(var i = 0; i< nbCard; i++){
+    for (var i = 0; i < nbCard; i++) {
         var cards = $("<img>")
         $(divBack).append(cards)
         cards.css({
-                "width": "160px",
-                "height": "240px",
-                "margin": "10px"
-        }).attr("src","Img/6.jpg").addClass("back")
+            "width": "160px",
+            "height": "240px",
+            "margin": "10px"
+        }).attr("src", "Img/6.jpg").addClass("back")
 
     }
 
 
 
-    var srcImg = ["Img/alban.png","Img/alex.jpg","Img/aurelie.jpg","Img/caro.jpg","Img/dimitri.jpg","Img/dorian.jpg","Img/fabien.jpg","Img/gaetan.png","Img/hermelen.jpg","Img/jb.jpg","Img/jd.jpg","Img/mika.jpg","Img/perrine.jpg","Img/quent.jpg","Img/thomas.jpg","Img/tibau.jpg","Img/tibausensei.png","Img/yann.jpg"];
+    var srcImg = ["Img/alban.png", "Img/alex.jpg", "Img/aurelie.jpg", "Img/caro.jpg", "Img/dimitri.jpg", "Img/dorian.jpg", "Img/fabien.jpg", "Img/gaetan.png", "Img/hermelen.jpg", "Img/jb.jpg", "Img/jd.jpg", "Img/mika.jpg", "Img/perrine.jpg", "Img/quent.jpg", "Img/thomas.jpg", "Img/tibau.jpg", "Img/tibausensei.png", "Img/yann.jpg"];
 
 
 
-    for(var i = 0; i< nbCard/2; i++){
-        var randomImg = Math.floor(Math.random()*srcImg.length)
+    for (var i = 0; i < nbCard / 2; i++) {
+        var randomImg = Math.floor(Math.random() * srcImg.length)
 
-        for( j = 0; j < 2; j++){
+        for (j = 0; j < 2; j++) {
             var cardsFace = $("<img>")
             cardsFace.css({
                 "width": "160px",
                 "height": "240px",
                 "margin": "10px",
-                "opacity": "1",
-            }).attr("src",srcImg[randomImg]).addClass("front")
-            $(divFace).append(cardsFace)
-
-
+                "opacity": "0",
+            }).attr("src", srcImg[randomImg]).addClass("front")
+            tableCards.push(cardsFace)
 
         }
 
+    };
+
+
+    shuffle(tableCards)
+
+    for (var i = 0; i < tableCards.length; i++) {
+        divFace.append(tableCards[i])
     }
+    console.log(tableCards);
 
+    $(".front").click(function() {
+        $(this).css("opacity", "1");
+    });
 
-    // for (var i = 0; i < nbCard; i++) {
-    //     cardsFace = document.createElement("img");
-    //     divFace.appendChild(cardsFace);
-    //     cardsFace.className = "front";
-    //     cardsFace.style.width = "160px";
-    //     cardsFace.style.height = "240px";
-    //     cardsFace.src = "/Img/alban.png"
-    //     cardsFace.style.margin = "10px";
-    //
-    //     cardsFace.style.opacity = "0"
-    // }
-
-
-    for (var i = 0; i < nbCard; i++) {
-
-        this.addEventListener("click", flip);
-
-        function flip() {
-
-            cardsFace.style.opacity = "1";
-            // table.push(this.parentNode.id);
-
-
-
-
+    function shuffle(a) {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
         }
+        return a;
     }
-
-
-
-
-
-
-
 
 
 
@@ -113,28 +93,6 @@ $(document).ready(function(){
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -200,27 +158,27 @@ $(document).ready(function(){
 
 
 
-    // for (var i = 0; i< img.length; i++){
-    //     // img[i].src = "Img/6.jpg";
-    //     // img[i].classList.remove("gne");
-    //     // img[i].classList.replace("gne","back");
-    //     // this.style.toggleClass("active");
-    //     this.addEventListener("click", flip);
-    //
-    //     function flip(){
-    //
-    //         // img[i].classList.replace("gne","back");7
-    //
-    //
-    //
-    //
-    //               // if(numFlip > 3){
-    //               //       var unmFlip += 1 ;
-    //               //
-    //               // }
-    //
-    //
-    //           }
-    //
-    // //
-    // }
+// for (var i = 0; i< img.length; i++){
+//     // img[i].src = "Img/6.jpg";
+//     // img[i].classList.remove("gne");
+//     // img[i].classList.replace("gne","back");
+//     // this.style.toggleClass("active");
+//     this.addEventListener("click", flip);
+//
+//     function flip(){
+//
+//         // img[i].classList.replace("gne","back");7
+//
+//
+//
+//
+//               // if(numFlip > 3){
+//               //       var unmFlip += 1 ;
+//               //
+//               // }
+//
+//
+//           }
+//
+// //
+// }
